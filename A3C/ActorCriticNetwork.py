68c0e4664d5431e7ffa98a564e3d972501b6a19f
QueensGambit <curry-berry@freenet.de>
2018-11-20 09:11:30 +0100

    added A2C version with tensorboard-support, losses seem to converge but reward doesn't increase

--- A3C/ActorCriticNetwork.py
+++ A3C/ActorCriticNetwork.py
@@ -20,31 +20,52 @@ class ActorCriticNetwork(torch.nn.Module):
         self.action_space = action_space
 
         # network architecture specification
-        fc1_out = 128 * 2
-        fc2_out = 128 * 2
+        n_hidden_units = 128
 
-        self.fc1 = nn.Linear(n_inputs, fc1_out)
-        #self.fc2 = nn.Linear(fc1_out, fc2_out)
+        #self.fc1 = nn.Linear(n_inputs, fc1_out)
+
+        self.stem = nn.Sequential(
+            nn.Linear(n_inputs, n_hidden_units),
+            nn.ReLU(),
+        )
 
         # Define the two heads of the network
         # -----------------------------------
 
         # * Value head
         # The value head has only 1 output
-        self.critic_linear = nn.Linear(fc2_out, 1)
+        self.critic_value = nn.Sequential(
+            nn.Linear(n_hidden_units, n_hidden_units),
+            nn.ReLU(),
+            nn.Linear(n_hidden_units, 1),
+            nn.Tanh(),
+        )
 
         # * Policy head
         # Define the number of output for the policy
         n_outputs = action_space.n if isinstance(action_space, Discrete) else action_space.shape[0]
 
         if self.is_discrete:
-            # in the dicrete case it has
-            self.actor_linear = nn.Linear(fc2_out, n_outputs)
+            # in the discrete case it uses as many outputs as there are discrete actions
+            self.actor_policy = nn.Sequential(
+                nn.Linear(n_hidden_units, n_hidden_units),
+                nn.ReLU(),
+                nn.Linear(n_hidden_units, n_outputs),
+                # note: The softmax activation will be applied during training
+            )
         else:
             # in the continuous case it has one output for the mu and one for the sigma variable
             # later the workers can sample from a normal distribution
-            self.mu = nn.Linear(fc1_out, n_outputs)
-            self.sigma = nn.Linear(fc1_out, n_outputs)
+            # the test worker takes action according to the mu value
+            self.actor_mu = nn.Sequential(
+                nn.Linear(n_hidden_units, n_outputs),
+                nn.Tanh(),
+            )
+
+            self.actor_variance = nn.Sequential(
+                nn.Linear(n_hidden_units, n_outputs),
+                nn.Softplus(),
+            )
 
         # initialize the weights
         self.apply(init_weights)
@@ -58,27 +79,29 @@ class ActorCriticNetwork(torch.nn.Module):
         :return: In the discrete case: value, policy
                  In the continuous case: value, mu, sigma
         """
-        inputs = inputs.float()
+        stem_out = self.stem(inputs)
 
         if self.is_discrete:
-            x = self.fc1(inputs)
+            #x = self.fc1(inputs)
             #x = F.relu6(x)
-            x = F.relu(x)
+            #x = F.relu(x)
             #x = self.fc2(x)
             #x = F.relu(x)
 
             # return torch.tanh(self.critic_linear(x)), self.actor_linear(x)
-            return self.critic_linear(x), self.actor_linear(x)
+            return self.critic_value(stem_out), self.actor_policy(stem_out)
 
         else:
 
-            x = self.fc1(inputs)
-            x = F.relu(x)
+            #x = self.fc1(inputs)
+            #x = F.relu(x)
             bound = torch.from_numpy(self.action_space.high)
-            mu = bound * torch.tanh(self.mu(x))
+            #mu = bound * torch.tanh(self.mu(x))
             # print("Mu scaled:", mu.data)
-            sigma = F.softplus(self.sigma(x)) + 1e-5
+            #sigma = F.softplus(self.sigma(x)) + 1e-5
             #return torch.tanh(self.critic_linear(x)), mu, sigma
-            value = torch.tanh(self.critic_linear(x))
+            #value = torch.tanh(self.critic_linear(x))
             #value = self.critic_linear(F.relu6(self.fc2(inputs)))
-            return value, mu, sigma
+            #return value, mu, sigma
+
+            return self.critic_value(stem_out), bound * self.actor_mu(stem_out), self.actor_variance(stem_out)
