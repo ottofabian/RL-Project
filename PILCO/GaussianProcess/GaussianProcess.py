fixed bug in beta computation, Controller test is not working

PILCO/GaussianProcess/GaussianProcess.py
@@ -101,7 +101,7 @@ class GaussianProcess(object):
     def predict(self, x):
         # K_trans = self.kernel(self._wrap_kernel_hyperparams(), x)
         # y_mean = K_trans.dot(self.betas)
-        return (np.mean(self.y, axis=0) + self.betas.T @ self.kernel(self._wrap_kernel_hyperparams(), x, self.X)[
+        return (np.mean(self.y, axis=0) + self.betas @ self.kernel(self._wrap_kernel_hyperparams(), x, self.X)[
             0]).flatten()
 
     def log_marginal_likelihood(self, hyp):
@@ -120,9 +120,13 @@ class GaussianProcess(object):
         self.K = self.kernel(params, self.X)[0]
 
         # learned variance from evidence maximization
-        noise = np.identity(self.X.shape[0]) * self.sigma_eps
-        self.K_inv = np.linalg.solve(self.K + noise, np.identity(self.K.shape[0]))
-        self.betas = self.K @ self.y
+
+        # noise is already added with WhiteNoiseKernel
+        # noise = np.identity(self.X.shape[0]) * self.sigma_eps
+        # self.K_inv = np.linalg.solve(self.K + noise, np.identity(self.K.shape[0]))
+
+        self.K_inv = np.linalg.solve(self.K, np.identity(self.K.shape[0]))
+        self.betas = np.linalg.solve(self.K, self.y).T
 
         # -------------------------------
         # TODO: Prob better, but autograd has no solve_triangular

PILCO/GaussianProcess/MultivariateGP.py
@@ -53,13 +53,14 @@ class MultivariateGP(object):
         # ----------------------------------------------------------------------------------------------------
         # Helper
 
-        beta = np.vstack([gp.betas.T for gp in self.gp_container]).T
+        beta = np.vstack([gp.betas for gp in self.gp_container]).T
         length_scales = self.get_length_scales()
         sigma_f = self.get_sigma_fs().reshape(self.n_targets)
 
         precision_inv = np.stack([np.diag(np.exp(-l)) for l in length_scales])
         precision_inv2 = np.stack([np.diag(np.exp(-2 * l)) for l in length_scales])
 
+        # centralized inputs
         diff = self.X - mu
         diff_scaled = np.expand_dims(diff, axis=0) / np.expand_dims(np.exp(2 * length_scales), axis=1)
 
@@ -86,7 +87,7 @@ class MultivariateGP(object):
             target_dim, state_dim).T * coefficient
 
         # ----------------------------------------------------------------------------------------------------
-        # compute predictive covariance
+        # compute predictive covariance, non-central moments
 
         k = 2 * sigma_f.reshape(target_dim, 1) - np.sum(zeta_a ** 2, axis=2) * .5
 
@@ -113,12 +114,14 @@ class MultivariateGP(object):
         Q = np.exp(k[:, np.newaxis, :, np.newaxis] + k[np.newaxis, :, np.newaxis, :] + mahalanobis_dist)
 
         if self.is_policy:
+            # noise for numerical reasons
             cov = scaling_factor * np.einsum('ji,iljk,kl->il', beta, Q, beta) + 1e-6 * np.identity(target_dim)
         else:
             cov = np.einsum('ji,iljk,kl->il', beta, Q, beta)
             trace = np.hstack([np.sum(Q[i, i] * gp.K_inv) for i, gp in enumerate(self.gp_container)])
             cov = (cov - np.diag(trace)) * scaling_factor + np.diag(np.exp(2 * sigma_f))
 
+        # Centralize moments
         cov = cov - mean[:, np.newaxis] @ mean[np.newaxis, :]
 
         return mean, cov, input_output_cov

PILCO/GaussianProcess/RBFNetwork.py
@@ -36,13 +36,10 @@ class RBFNetwork(GaussianProcess):
         return params
 
     def compute_matrices(self):
-
-        params = self._wrap_kernel_hyperparams()
-        K = self.kernel(params, self.X)[0]
-
-        # Interpreting the RBF network as deterministic GP, the inverse of K has to be eliminated
-        self.K_inv = np.zeros(K.shape)
-        self.betas = K @ self.y
+        # Interpreting the RBF network as deterministic GP, the inverse of K and is not relevant for the computation,
+        # therefore it is set to 0
+        super(RBFNetwork, self).compute_matrices()
+        self.K_inv = np.zeros(self.K.shape)
 
     def unwrap_params(self, params):
 

PILCO/Kernels/RBFKernel.py
@@ -12,19 +12,30 @@ class RBFKernel(Kernel):
         self.num_hyp = lambda x: np.size(x, 1) + 1
 
     def __call__(self, log_hyperparam, x, z=None):
-
         log_hyperparam = np.atleast_2d(log_hyperparam)
 
         length_scales = np.exp(log_hyperparam[:, :x.shape[1]])
         sigma_f = np.exp(2 * log_hyperparam[:, x.shape[1]])
         sigma_f = sigma_f.reshape(-1, 1, 1)
+        # sigma_f = sigma_f.reshape(-1, 1)
 
         # datapoints x dimension
         scaled_x = np.expand_dims(x, 0) / np.expand_dims(length_scales, 1)
         if z is None:
-            diff = np.expand_dims(scaled_x, 1) - np.expand_dims(scaled_x, 2)
+            diff_a = np.expand_dims(scaled_x, 1)
+            diff_b = np.expand_dims(scaled_x, 2)
         else:
             scaled_z = np.expand_dims(z, 0) / np.expand_dims(length_scales, 1)
-            diff = np.expand_dims(scaled_x, 1) - np.expand_dims(scaled_z, 2)
+            diff_a = np.expand_dims(scaled_x, 1)
+            diff_b = np.expand_dims(scaled_z, 2)
 
-        return sigma_f * np.exp(-0.5 * np.sum(diff ** 2, axis=3))
+        # The maha call without Q just computes the squared error
+        # mahalanobis_dist = np.expand_dims(np.sum(diff_a * diff_a, axis=-1), axis=-1) + np.expand_dims(
+        #     np.sum(diff_b * diff_b, axis=-1), axis=-2) - 2 * np.einsum('...ij, ...kj->...ik', diff_a, diff_b)
+
+        # return sigma_f * np.exp(-.5 * np.sum(mahalanobis_dist, axis=3))
+        # return np.exp(2 * log_hyperparam[:, x.shape[1]] - 0.5 * np.sum((diff_a - diff_b) ** 2, axis=3))
+        return np.exp(2 * log_hyperparam[:, x.shape[1]]) * np.exp(-0.5 * np.sum((diff_a - diff_b) ** 2, axis=3))
+
+        # a = (x / length_scales)
+        # return sigma_f * np.exp(-.5 * (np.sum(a * a, axis=1) + np.sum(a * a, axis=1) - 2 * a@a.T))

PILCO/PILCO.py
@@ -125,7 +125,7 @@ class PILCO(object):
             done = False
 
             while not done and i < n_init:
-                # self.env.render()
+                self.env.render()
                 action = self.env.action_space.sample()
                 state, reward, done, _ = self.env.step(action)
 
@@ -272,16 +272,16 @@ class PILCO(object):
 
             # sample over dist
             # x = np.random.multivariate_normal(state_action_mu, state_action_cov, size=1000)
-            if np.any(np.isnan(state_action_cov)) or np.any(np.isnan(state_action_mu)):
-                print(state_action_cov)
-                print(state_action_mu)
-                print("nan")
+            # if np.any(np.isnan(state_action_cov)) or np.any(np.isnan(state_action_mu)):
+            #     print(state_action_cov)
+            #     print(state_action_mu)
+            #     print("nan")
 
-            x = []
-            for _ in range(100):
-                # reparametrization trick
-                x.append(np.random.randn(len(state_action_mu)) @ state_action_cov + state_action_mu)
-            x = np.array(x)
+            # x = []
+            # for _ in range(100):
+            #     # reparametrization trick
+            #     x.append(np.random.randn(len(state_action_mu)) @ state_action_cov + state_action_mu)
+            # x = np.array(x)
             # # use real env for dynamics
             # pred = []
             # self.env.reset()
@@ -296,11 +296,11 @@ class PILCO(object):
             # pred = np.array(pred).T
 
             # use deterministic prediction on samples one real GP dynamics
-            pred = self.dynamics_model.predict(x)
+            # pred = self.dynamics_model.predict(x)
 
-            delta_mu = np.mean(pred, axis=1)
-            diff = pred - delta_mu[:, None]
-            delta_cov = 1 / (diff - 1) * diff @ diff.T
+            # delta_mu = np.mean(pred, axis=1)
+            # diff = pred - delta_mu[:, None]
+            # delta_cov = 1 / (diff - 1) * diff @ diff.T
 
             # delta_cov = np.cov(pred)
 

PILCO/Test/Controller_Test.py
@@ -26,7 +26,7 @@ def test_rbf():
     X0 = np.random.rand(100, state_dim)
     A = np.random.rand(state_dim, n_actions)
     Y0 = np.sin(X0).dot(A) + 1e-3 * (np.random.rand(100, n_actions) - 0.5)  # Just something smooth
-    length_scales = np.ones((state_dim,))
+    length_scales = np.full((state_dim,), 5)
 
     rbf = RBFController(n_actions=n_actions, n_features=n_features, rollout=None, length_scales=length_scales)
     rbf.fit(X0, Y0)
@@ -36,10 +36,10 @@ def test_rbf():
     s = np.random.rand(state_dim, state_dim)
     s = s.dot(s.T)  # Make s positive semidefinite
 
-    M, S, V = compute_action_wrapper(rbf, m, s)
+    M, S, V = rbf.choose_action(m, s, False)
 
     # convert data to the struct expected by the MATLAB implementation
-    length_scales = rbf.get_length_scales()
+    length_scales = length_scales.reshape(n_actions, state_dim)
     sigma_f = rbf.get_sigma_fs()
     sigma_eps = rbf.get_sigma_eps()
 
@@ -55,7 +55,7 @@ def test_rbf():
     gpmodel.targets = Y0
 
     # Call gp0 in octave
-    M_mat, S_mat, V_mat = octave.gp2(gpmodel, m.T, s, nout=3)
+    M_mat, S_mat, V_mat = octave.gp2_debug(gpmodel, m.T, s, nout=3)
     M_mat = np.asarray([M_mat])
     S_mat = np.atleast_2d(S_mat)
     V_mat = np.asarray(V_mat)